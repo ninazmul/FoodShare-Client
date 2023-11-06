6d1eb66605ae6f13a7714581327ea2fcbb3b5941
ninazmul <nazmulsaw@gmail.com>
2023-11-06 15:38:28 +0600

    filter by name on available page done

--- src/Pages/AvailableFood.jsx
+++ src/Pages/AvailableFood.jsx
@@ -1,36 +1,55 @@
 import { useEffect, useState } from "react";
 import AvailableFoodCard from "./AvailableFoodCard";
-
+import { BsSearch } from "react-icons/bs";
 
 const AvailableFood = () => {
-
   const [available, setAvailable] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
-    fetch('available.json')
-      .then(res => res.json())
-      .then(data => setAvailable(data));
-  },[])
+    fetch("available.json")
+      .then((res) => res.json())
+      .then((data) => setAvailable(data));
+  }, []);
 
-    return (
-      <div>
-        <div className="py-6">
-          <h1 className="text-5xl text-pink-700 font-bold text-center">
-            Available Foods: {available.length}
-          </h1>
-          <p className="text-center">
-            Explore the list of available food items from our generous community
-            members. If you're in need, you can request these food items to help
-            fulfill your hunger.
-          </p>
-        </div>
-        <div className="grid grid-cols-3 gap-4">
-          {
-            available.map(food => <AvailableFoodCard key={food._id} food={food}></AvailableFoodCard>)
-          }
+  // Filter available foods based on the search query
+  const filteredAvailableFoods = available.filter((food) =>
+    food.foodName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="py-6">
+        <h1 className="text-5xl text-pink-700 font-bold text-center">
+          Available Foods: {available.length}
+        </h1>
+        <p className="text-center">
+          Explore the list of available food items from our generous community
+          members. If you're in need, you can request these food items to help
+          fulfill your hunger.
+        </p>
+        <div className="mt-4 mb-4 text-center ">
+          
+          <input
+            type="text"
+            placeholder="Search by Food Name"
+            className="input input-bordered border-pink-700 border-2 w-full max-w-xs relative"
+            value={searchQuery}
+            onChange={handleSearch}
+          />
         </div>
       </div>
-    );
+      <div className="grid grid-cols-3 gap-4">
+        {filteredAvailableFoods.map((food) => (
+          <AvailableFoodCard key={food._id} food={food}></AvailableFoodCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AvailableFood;
